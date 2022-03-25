8ecbca1d7add790a71de6e0b8e85fc71ad6be3ca
Mohd Siddiqui <Waseque.MohdSiddiqui.ctr@sabre.com>
2022-03-25 18:35:07 +0530

    First commit

---
+++ APP.py
@@ -0,0 +1,101 @@
+##
+import dash
+from dash import dcc, Output, Input
+from dash import html
+import pandas as pd
+import investpy as inv
+
+##
+df = inv.get_index_historical_data(index="Nifty 50", country="India", from_date='01/01/2018', to_date='01/01/2022')
+##
+df['Date'] = pd.to_datetime(df.index)
+##
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+                "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title = "Nifty50 Analytics understanding the maverick market!"
+app.layout = html.Div(
+    children=[
+        html.H1(children="Nifty 50 Analytics", style={"fontSize":"32px", "color":"blue", "text-align":"center"}),
+        html.P(
+            children="Analyze Nifty 50 price Trend with Volume", className="header-description"
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children="Date Range",
+                    className="menu-title"
+                ),
+                dcc.DatePickerRange(
+                    id="date-range",
+                    min_date_allowed=df.Date.min().date(),
+                    max_date_allowed=df.Date.max().date(),
+                    start_date=df.Date.min().date(),
+                    end_date=df.Date.max().date(),
+                )
+            ], className="menu"
+        ),
+        dcc.Graph(
+            id="price-chart"
+        ),
+        dcc.Graph(
+            id="volume-chart"
+        ),
+    ]
+)
+@app.callback(
+    [Output("price-chart", "figure"), Output("volume-chart", "figure")],
+    [
+        Input("date-range", "start_date"),
+        Input("date-range", "end_date"),
+    ],
+)
+def update_charts(start_date, end_date):
+    mask = (
+            (df.Date >= start_date)
+            & (df.Date <= end_date)
+    )
+    filtered_data = df.loc[mask, :]
+    price_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Close"],
+                "type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Close of Nifty50",
+            },
+            "colorway": ["#17B897"],
+        },
+    }
+
+    volume_chart_figure = {
+        "data": [
+            {
+                "x": filtered_data["Date"],
+                "y": filtered_data["Volume"],
+                "type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Volume Nifty50",
+            },
+            "colorway": ["#E12D39"],
+        },
+    }
+    return price_chart_figure, volume_chart_figure
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
